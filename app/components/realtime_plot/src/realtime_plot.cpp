d2ef35a6c0b8ab9b37f73d296ec95e4d8627900b
sarath-menon <sarathmenon.downey@gmail.com>
2021-11-15 18:21:08 +0100

    added page for realtime plotter

--- app/components/realtime_plot/src/realtime_plot.cpp
+++ app/components/realtime_plot/src/realtime_plot.cpp
@@ -1,14 +1,9 @@
 #include "realtime_plot.h"
 #include "ui_realtime_plot.h"
 
-realtime_plot::realtime_plot(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::realtime_plot)
-{
-    ui->setupUi(this);
+realtime_plot::realtime_plot(QWidget *parent)
+    : QWidget(parent), ui(new Ui::realtime_plot) {
+  ui->setupUi(this);
 }
 
-realtime_plot::~realtime_plot()
-{
-    delete ui;
-}
+realtime_plot::~realtime_plot() { delete ui; }
